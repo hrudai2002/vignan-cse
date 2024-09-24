4a422b99c739f1893cf0c31bccfe4e0ce4c9ab5d
hrudai2002 <hrudai2002@gmail.com>
2024-09-24 22:35:21 +0530

    removed environemnt.ts

--- environment.ts
+++
@@ -1,7 +0,0 @@
-const developmentEnvironment = {
-    apiUrl: 'http://ec2-65-2-74-196.ap-south-1.compute.amazonaws.com:8001'
-    // apiUrl: 'http://localhost:8001'
-
-}
-
-export const environments = developmentEnvironment
